957678eddb97f585c82cda1f8f637f452f78b9b5
jeebeshchandrapodder0 <jeebeshchandrapodder0@gmail.com>
2025-07-29 13:43:08 +0530

    Data Ingestion Code Commented for Explanation and Readability

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,16 +1,39 @@
 import os
-import utils.model_loader as ModelLoader
+import sys
+from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
-from exception.custom_exception import DocumentProcessingError
+from exception.custom_exception import DocumentPortalException
 from model.models import *
-
-from langchain_core.output_parsers import JSONOutputParser
+from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import *
 
 class DocumentAnalyzer:
-
+    """
+    Analyzes documents using a pre-trained model.
+    Automatically logs all actions and supports session-based organization.
+    """
     def __init__(self):
-        pass
-
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.loader=ModelLoader()
+            self.llm=self.loader.load_llm()
+            
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+            
+            self.prompt = prompt
+            
+            self.log.info("DocumentAnalyzer initialized successfully")
+            
+            
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error in DocumentAnalyzer initialization", sys)
+        
+        
+    
     def analyze_document(self):
         pass
+    
